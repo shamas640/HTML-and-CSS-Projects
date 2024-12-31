a31b797cb4554b06e434577bbbb998501a431d5d
Shamas <shonsole@hotmail.com>
2024-12-31 20:23:13 +0000

    basic face and size done

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello, World!");
+
+function My_First_Functio(){
+	     var String = "kiss me, I'm Irish!";
+		 var result = String.fontcolor("green");
+		 document.getElementById("Irish").innerHTML= result;
+	}
